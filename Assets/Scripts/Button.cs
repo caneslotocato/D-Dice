f5961125a35beb086f6e6c07b10d7a912f9c153c
caneslotocato <caneslotocato1@gmail.com>
2016-07-09 10:10:25 +0200

    projecty

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public GameObject dice;
+    private Transform currentDice;
+
+
+    void Awake()
+    {
+        currentDice = GameObject.Find("Current Dice").transform;
+        
+    }
+
+
+    public void SetDice()
+    {
+        dice.transform.position = currentDice.position;
+        //currentDice.transform.position = new Vector3(5000, 5000, 5000);
+        //currentDice.GetComponent<Rigidbody>().useGravity = false;
+        
+        currentDice.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+
+
+	
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private float forceToAdd;
+    public float maxRange;
+    public float start;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        forceToAdd = Random.Range(start - maxRange, start + maxRange);
+        WatchRaycast(forceToAdd);
+	}
+
+    void WatchRaycast(float force)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                hit.rigidbody.useGravity = true;
+                hit.rigidbody.AddForceAtPosition(ray.direction * force, hit.point);
+            }
+        }
+    }
+}
