81f23983881ed09d4b3604c53d02a82d08533371
caneslotocato <caneslotocato1@gmail.com>
2016-07-10 22:17:06 +0200

    I think the end

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -3,24 +3,47 @@ using System.Collections;
 
 public class Button : MonoBehaviour {
 
-    public GameObject dice;
+    //public GameObject dice;
+    public int myDice;
     private Transform currentDice;
 
 
     void Awake()
     {
         currentDice = GameObject.Find("Current Dice").transform;
-        
+        //dice.SetActive(false);
+        //GameController.instance.allDice[myDice].SetActive(false);
     }
 
 
     public void SetDice()
     {
-        dice.transform.position = currentDice.position;
-        //currentDice.transform.position = new Vector3(5000, 5000, 5000);
-        //currentDice.GetComponent<Rigidbody>().useGravity = false;
-        
-        currentDice.GetComponent<Rigidbody>().useGravity = false;
+        /*
+        //currentDice = null;
+        //currentDice.SetActive(false);
+        //Destroy(currentDice);
+        GameObject cane = Instantiate(dice, dice.transform.position, dice.transform.rotation)as GameObject;
+        Destroy(dice);
+        //currentDice = dice;
+        cane.GetComponent<Rigidbody>().useGravity = false;
+        cane.SetActive(true);
+        DontDestroyOnLoad(cane);
+        Application.LoadLevel(Application.loadedLevel);
+        */
+        for (int i = 0; i < GameController.instance.allDice.Length; i++)
+        {
+            GameController.instance.allDice[i].SetActive(false);
+        }
+        GameController.instance.allDice[myDice].GetComponent<Rigidbody>().useGravity = false;
+        GameController.instance.allDice[myDice].SetActive(true);
+        float a = Random.Range(0, 360);
+        float b = Random.Range(0, 360);
+        float c = Random.Range(0, 360);
+        GameController.instance.allDice[myDice].transform.rotation = Quaternion.Euler(a, b, c);
+        GameController.instance.allDice[myDice].transform.position = currentDice.position;
+        GameController.instance.allDice[myDice].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+
     }
 
 
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+    
+    public GameObject[] allDice;
     private float forceToAdd;
     public float maxRange;
     public float start;
+    public static GameController instance;
 
+
+    void Awake()
+    {
+        instance = this;
+        for(int i = 0; i < allDice.Length; i++)
+        {
+            allDice[i].SetActive(false);
+        }
+    }
     // Use this for initialization
     void Start () {
 	
@@ -26,8 +38,10 @@ public class GameController : MonoBehaviour {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
+                hit.rigidbody.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
                 hit.rigidbody.useGravity = true;
                 hit.rigidbody.AddForceAtPosition(ray.direction * force, hit.point);
+                
             }
         }
     }
